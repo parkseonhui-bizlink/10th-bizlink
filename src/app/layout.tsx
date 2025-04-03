c6362cb5a623f470d07b9b1cf11c2064a55ad5b4
parkseonhui <seonhui.park@bizlink.io>
2025-04-03 14:07:56 +0900

    fix: 修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,30 @@ export const metadata: Metadata = {
   title: 'Bizlink10周年記念サイト',
   description:
     '皆さまの夢と共に、未来へ。私たちの始まりは、間取り5坪の小さなオフィスでした。 そこから10年――。日本初のIT人材プラットフォーム企業として、独自性ある技術を磨きながら刻んできた年月。',
+  openGraph: {
+    title: 'Bizlink10周年記念サイト',
+    description:
+      '皆さまの夢と共に、未来へ。日本初のIT人材プラットフォーム企業として、独自性ある技術を磨きながら刻んできた年月。',
+    url: 'https://10th.bizlink.io/',
+    siteName: 'Bizlink',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Bizlink10周年記念ビジュアル',
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Bizlink10周年記念サイト',
+    description:
+      '皆さまの夢と共に、未来へ。IT人材プラットフォーム企業として歩んできた10年。',
+    images: ['/twitter-image.png'],
+  },
 }
 
 export default function RootLayout({
